b4f064af74eb04df677ead48f5f040f37d1f06a1
Bruno César da Silva <57082742+bcs018@users.noreply.github.com>
2026-08-05 17:30:23 -0300

    Criando migrations

---
+++ chatbot/database/migrations/2026_08_05_200753_create_bots_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bots', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('empresa_id')
+                  ->constrained('empresas')
+                  ->cascadeOnDelete();
+            $table->string('nome');
+            $table->text('prompt_base')->nullable();
+            $table->boolean('ativo')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bots');
+    }
+};
